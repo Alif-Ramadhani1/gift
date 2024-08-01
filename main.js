75ae57bc6d925c6bda9027bb1946efd4f3b66c88
alif ramadhani <aliframadhani792@gmail.com>
2024-08-01 21:33:58 +0700

    ubah: penambahan lagu

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-onload = () =>{
-        document.body.classList.remove("container");
+let audio;
+
+function setupBackgroundMusic() {
+    audio = new Audio('sound/Taylor Swift - Lover.mp3');
+    audio.loop = true;
+    audio.volume = 0.5;
+}
+
+function playBackgroundMusic() {
+    if (audio) {
+        audio.play().catch(error => {
+            console.log('Gagal memutar audio:', error);
+        });
+    }
+}
+
+window.onload = () => {
+    document.body.classList.remove("container");
+    setupBackgroundMusic();
+    
+    // Contoh: memulai musik saat pengguna mengklik di mana saja di halaman
+    document.body.addEventListener('click', playBackgroundMusic, { once: true });
 };
